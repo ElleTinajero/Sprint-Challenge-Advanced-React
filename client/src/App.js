f8727bdb7520d32e5f27c994da2c133c33e5bdf8
Michelle Tinajero <elletinajero11@gmail.com>
2020-04-19 19:29:08 -0500

    added components & hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {Players} from "./components/players";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="appContainer">
+      <div data-testid="navbar" className="navContainer">
+        <Navbar/>
+      </div>
+      <div>
+        <Players/>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ client/src/components/players.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import axios from 'axios';
+
+export class Players extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            player: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:5000/api/players")
+        .then(res => {
+            this.setState({
+                player: res.data
+            })
+        })
+    }
+    
+    render() {
+        return(
+            <div data-testid="player">
+                {this.state.player.map(p => 
+                    <div className="players" key={p.id}>
+                    <h2>Name: {p.name}</h2>
+                    <h2>Country: {p.name}</h2>
+                    <h2>Searches: {p.searches}</h2>
+                    </div>)}
+            </div>
+        )
+    }
+}
